b4b4fc0c8c8c7f85e69712b2da29df8a3aa0134e
John Glenn II (desktop) <johncglenn2@gmail.com>
2026-05-19 13:41:31 -0400

    Update startup.m

--- code_simulink/startup.m
+++ code_simulink/startup.m
@@ -1,2 +1,4 @@
+% Add code_simulink & subfolders to the MATLAB path
 currentPath = fileparts(mfilename('fullpath'));
 codePath = fullfile(currentPath, '..', 'code_simulink');
+addpath(genpath(codePath));
